ad2dd25ba7d9ee4395ea6a974c289e558baa56a6
Andreas Jepsen <a.jepsen20@gmail.com>
2021-09-13 14:05:05 +0200

    .

--- src/components/pages/FrontPage/Frontpage.jsx
+++ src/components/pages/FrontPage/Frontpage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import image from './assets/hero_image.png';
 
 export function Frontpage() {
 
   
   return (
     <div>
-
+<img src={image} height={100} width={100} />
 </div>
   )
 }
